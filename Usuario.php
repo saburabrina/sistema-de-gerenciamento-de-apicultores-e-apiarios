97913c16e31c89b81349b2021d67dd5f5e9bb087
lhamasm <44074705+lhamasm@users.noreply.github.com>
2019-05-20 20:26:10 -0300

    Add files via upload

--- Usuario.php
+++ Usuario.php
@@ -2,6 +2,14 @@
 
 	require_once('DataGetter.php');
 	require_once('Apicultor.php');
+	require_once('Apiario.php');
+	require_once('Amostra.php');
+	require_once('Caixa.php');
+	require_once('Colmeia.php');
+	require_once('Endereco.php');
+	require_once('Fumegador.php');
+	require_once('MedicoesClimaticas.php');
+	require_once('Propriedade.php');
 
 	class Usuario {
 		private $nome;
@@ -34,28 +42,16 @@
 	}
 
 	public function recuperarApiarios(){
-		$stmt = DataGetter::getConn()->prepare("SELECT APIARIO.nome as nome, CONCAT(APIARIO.dono, '-', APICULTOR.nome) as dono, CONCAT(ENDERECO.logradouro, ',', ENDERECO.numero, ',', ENDERECO.complemento, ',', ENDERECO.comunidade, ',', ENDERECO.bairro, ',', ENDERECO.cidade, ',', ENDERECO.estado, ',', ENDERECO.cep) as endereco, APIARIO.inscricao_estadual as inscricao_estadual, APIARIO.data_fundacao as data_fundacao, APIARIO.tipo_florada as tipo_florada, APIARIO.latitude as latitude, APIARIO.longitude as longitude, APIARIO.expandida as expandida, APIARIO.problema_sanitario as problema_sanitario, APIARIO.num_caixas_povoadas as num_caixas_povoadas, APIARIO.num_caixas_vazias as num_caixas_vazias, APIARIO.tipo_instalacao as tipo_instalacao FROM APIARIO, APICULTOR, PROPRIEDADE, ENDERECO WHERE APIARIO.propriedade = PROPRIEDADE.id AND PROPRIEDADE.endereco = ENDERECO.id");
+		$stmt = DataGetter::getConn()->prepare("SELECT APIARIO.nome as nome, ENDERECO.id as id, ENDERECO.logradouro as logradouro, ENDERECO.numero as numero, ENDERECO.complemento as complemento, ENDERECO.comunidade as comunidade, ENDERECO.bairro as bairro, ENDERECO.cidade as cidade, ENDERECO.estado as estado, ENDERECO.cep as cep, APIARIO.inscricao_estadual as inscricao_estadual, APIARIO.data_fundacao as data_fundacao, APIARIO.tipo_florada as tipo_florada, APIARIO.latitude as latitude, APIARIO.longitude as longitude, APIARIO.expandida as expandida, APIARIO.problema_sanitario as problema_sanitario, APIARIO.num_caixas_povoadas as num_caixas_povoadas, APIARIO.num_caixas_vazias as num_caixas_vazias, APIARIO.tipo_instalacao as tipo_instalacao FROM APIARIO, APICULTOR, PROPRIEDADE, ENDERECO WHERE APIARIO.propriedade = PROPRIEDADE.id AND PROPRIEDADE.endereco = ENDERECO.id");
 		$stmt->execute();
 
 		$apiarios = array();
 		while($apiario = $stmt->setFetchMode(PDO::FETCH_ASSOC)){
-			array_push($apiarios, new Apiario($apiario['nome'], $apiario['dono'], $apiario['endereco'], $apiario['inscricao_estadual'], $apiario['data_fundacao'], $apiario['tipo_florada'], $apiario['latitude'], $apiario['logintude'], $apiario['expandida'], $apiario['problema_sanitario'], $apiario['num_caixas_povoadas'], $apiario['num_caixas_vazias'], $apiario['tipo_instalacao']));
+			array_push($apiarios, new Apiario($apiario['nome'], $apiario['dono'], new Endereco($apiario['id'], $apiario['logradouro'], $apiario['numero'], $apiario['complemento'], $apiario['comunidade'], $apiario['bairro'], $apiario['cidade'], $apiario['estado'], $apiario['cep']), $apiario['inscricao_estadual'], $apiario['data_fundacao'], $apiario['tipo_florada'], $apiario['latitude'], $apiario['logintude'], $apiario['expandida'], $apiario['problema_sanitario'], $apiario['num_caixas_povoadas'], $apiario['num_caixas_vazias'], $apiario['tipo_instalacao']));
 		}
 
 		return $apiarios;
 	}
-
-	/*public function recuperarApicultores(){
-		$stmt = DataGetter::getConn()->prepare("SELECT APICULTOR.cpf as cpf, APICULTOR.nome as nome, APICULTOR.certificacao as certificao, APICULTOR.email as email, APICULTOR.telefone as telefone, APICULTOR.producao_anual as producao_anual, APICULTOR.perfil as perfil, APICULTOR.vinculo as vinculo, CONCAT(ENDERECO.logradouro, ',', ENDERECO.numero, ',', ENDERECO.complemento, ',', ENDERECO.comunidade, ',', ENDERECO.bairro, ',', ENDERECO.cidade, ',', ENDERECO.estado, ',', ENDERECO.cep) as endereco, CONCAT(ENDERECO.logradouro, ',', ENDERECO.numero, ',', ENDERECO.complemento, ',', ENDERECO.comunidade, ',', ENDERECO.bairro, ',', ENDERECO.cidade, ',', ENDERECO.estado, ',', ENDERECO.cep) as localTrabalho FROM APICULTOR, PROPRIEDADE, ENDERECO WHERE APICULTOR.endereco = ENDERECO.id AND APICULTOR.trabalha_em = PROPRIEDADE.id AND PROPRIEDADE.endereco = (SELECT id FROM ENDERECO)");
-		$stmt->execute();
-
-		$apicultores = array();
-		while($apicultor = $stmt->setFetchMode(PDO::FETCH_ASSOC)){
-			array_push($apicultores, new Apicultor($apicultor['cpf'], $apicultor['nome'], $apicultor['certificacao'], $apicultor['email'], $apicultor['telefone'], $apicultor['producao_anual'], $apicultor['perfil'], $apicultor['vinculo'], $apicultor['endereco'], $apicultor['trabalha_em']));
-		}
-
-		return $apicultores;
-	}*/
 
 	public function recuperarApiariosPorApicultor($apicultor) {
 		$stmt = DataGetter::getConn()->prepare("SELECT APIARIO.nome as nome, APIARIO.dono as dono, APICULTOR.nome as nomeDono, ENDERECO.id as id, ENDERECO.logradouro as logradouro, ENDERECO.numero as numero, ENDERECO.complemento as complemento, ENDERECO.comunidade as comunidade, ENDERECO.bairro as bairro, ENDERECO.cidade as cidade, ENDERECO.estado as estado, ENDERECO.cep as cep, APIARIO.inscricao_estadual as inscricao_estadual, APIARIO.data_fundacao as data_fundacao, APIARIO.tipo_florada as tipo_florada, APIARIO.latitude as latitude, APIARIO.longitude as longitude, APIARIO.expandida as expandida, APIARIO.problema_sanitario as problema_sanitario, APIARIO.num_caixas_povoadas as num_caixas_povoadas, APIARIO.num_caixas_vazias as num_caixas_vazias, APIARIO.tipo_instalacao as tipo_instalacao FROM APIARIO, APICULTOR, PROPRIEDADE, ENDERECO GROUP BY APICULTOR.cpf HAVING APICULTOR.cpf = APIARIO.dono AND APICULTOR.cpf = " . $apicultor->getCpf());
@@ -376,6 +372,70 @@
 		$resultado = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
 		return $resultado['destino'];
+	}
+
+	public function recuperarControlesVeterinarios($apiario){
+		$stmt = DataGetter::getConn()->prepare("SELECT id, data_exame, condicao_vet_geral, nome_veterinario, crmv_veterinario FROM CONTROLE_VETERINARIO ORDER BY data_exame WHERE apiario = " . $apiario->getNome());
+
+		$stmt->execute();
+
+		$controles = array();
+		while($controle = $stmt->setFetchMode(PDO::FETCH_ASSOC)){
+			array_push($controles, new ControleVeterinario($controle['id'], $apiario->getNome(), $controle['data_exame'], $controle['condicao_vet_geral'], $controle['nome_veterinario'], $controle['crmv_veterinario']));
+		}
+
+		return $controles;
+	}
+
+	public function recuperarInformacoesVeterinarioControle($apiario, $data){
+		$stmt = DataGetter::getConn()->prepare("SELECT nome_veterinario, crmv_veterinario FROM CONTROLE_VETERINARIO WHERE data_exame = " . $data . " AND apiario = " . $apiario->getNome());
+
+		$stmt->execute();
+		$info = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+		return $info;
+	}
+
+	public function recuperarInformacoesVeterinarioTratamento($data){
+		$stmt = DataGetter::getConn()->prepare("SELECT nome_veterinario, crmv_veterinario FROM TRATAMENTO WHERE data_tratamento = " . $data);
+
+		$stmt->execute();
+		$info = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+		return $info;
+	}
+
+	public function recuperarTratamentosColmeias($apiario) {
+		$stmt = DataGetter::getConn()->prepare("SELECT TRATAMENTO.* FROM TRATAMENTO, CAIXA WHERE CAIXA.apiario = " . $apiario->getNome() . "AND CAIXA.colmeia = TRATAMENTO.colmeia");
+
+		$stmt->execute();
+
+		$tratamentos = array();
+		while($tratamento = $stmt->setFetchMode(PDO::FETCH_ASSOC)){
+			array_push($tratamentos, new Tratamento($tratamento['id'], $tratamento['quantidade_doses'], $tratamento['forma_dosagem'], $tratamento['doenca'], $tratamento['produto'], $tratamento['data_tratamento'], $tratamento['nome_veterinario'], $tratamento['crmv_veterinario']));
+		}
+
+		return $tratamentos;
+	}
+
+	public function colmeiasApresentaramProblema($apiario){
+		$stmt = DataGetter::getConn()->prepare("SELECT * FROM APIARIO WHERE EXISTS (SELECT TRATAMENTO.* FROM TRATAMENTO, CAIXA WHERE CAIXA.apiario = APIARIO.nome AND APIARIO.nome = " . $apiario->getNome() . " AND CAIXA.colmeia = TRATAMENTO.colmeia AND TRATAMENTO.doenca IS NOT NULL)");
+
+		$stmt->execute();
+		$apiario = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+		return count($apiario);
+	}
+
+	public function recuperarCondicaoVeterinaria($apiario){
+		$stmt = DataGetter::getConn()->prepare("SELECT data_exame, condicao_vet_geral FROM CONTROLE_VETERINARIO WHERE apiario = " . $apiario->getNome());
+
+		$condicoes = array();
+		while($condicao = $stmt->setFetchMode(PDO::FETCH_ASSOC)){
+			array_push($condicoes, $condicao);
+		}
+
+		return $condicoes;
 	}
 
 ?>
