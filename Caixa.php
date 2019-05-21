7411c073e1646387a8e4049fe17d907c77a7c389
Lara <lara.santannan@gmail.com>
2019-05-21 10:58:58 -0300

    add get and set caixa

--- Caixa.php
+++ Caixa.php
@@ -32,4 +32,36 @@
     public function setApiario($apiario){
         $this->apiario = $apiario;
 	}
+
+	public function getColmeia(){
+        return $this->colmeia;
+    }
+
+    public function setColmeia($colmeia){
+        $this->colmeia = $colmeia;
+	}
+
+	public function getMaterial(){
+        return $this->material;
+    }
+
+    public function setMaterial($material){
+        $this->material = $material;
+	}
+
+	public function getMelgueira(){
+        return $this->melgueira;
+    }
+
+    public function setMelgueira($melgueira){
+        $this->melgueira = $melgueira;
+	}
+
+	public function getLocalExtracao(){
+        return $this->local_extracao;
+    }
+
+    public function setLocalExtracao($local_extracao){
+        $this->local_extracao = $local_extracao;
+	}
 ?>
